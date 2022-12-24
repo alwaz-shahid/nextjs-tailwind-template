54db59fa94800e482eb6bb6c2058cd8418141652
Alwaz Shahid <52915358+Alwaz18@users.noreply.github.com>
2022-12-24 16:36:05 +0500

    ./pages => ./src/pages

---
+++ src/pages/_app.js
@@ -0,0 +1,16 @@
+import { DefaultSeo } from 'next-seo';
+import '../src/styles/globals.css';
+import { ThemeProvider } from 'next-themes';
+import SEO from '../../next-seo.config';
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <ThemeProvider attribute='class'>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default MyApp;
